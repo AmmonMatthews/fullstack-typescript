9a5f4f654740f4e086b59f5dbc714d6f628d69f8
AmmonMatthews <ammonmatthews@gmail.com>
2020-09-19 11:08:31 -0600

    notes in the index.ts file created

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-interface User {
-    name: string,
-    age: number, 
-    email: string
-}
+// interface User {
+//     name: string,
+//     age: number, 
+//     email: string
+// }
 
-const user = (userObj: User) => {
-    console.log(userObj.name)
-}
+// const user = (userObj: User) => {
+//     console.log(userObj.name)
+// }
 
-const obj = {
-    name: "Wendy", 
-    age: 20,
-    email: ""
-}
+// const obj = {
+//     name: "Wendy", 
+//     age: 20,
+//     email: ""
+// }
 
-user(obj)
+// user(obj)
+
+
+let helloWorld: string = "hello World!!!"
+console.log(helloWorld)
+
+// The biggest thing with typescript is that when the compiler runs the code it wilil catch all of the errors but will still compile data into the designated folder in the output folder. 
+
